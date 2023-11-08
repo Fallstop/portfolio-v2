de05bb2a0f0bceb69dc2f147fae4bc5e3df344b2
Jasper Miller-Waugh <contact@jmw.nz>
2023-11-09 12:32:07 +1300

    Start of authors inclusion

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import PrimaryLayout from '$lib/components/layout/PrimaryLayout.svelte';
-    import { NavigationOption } from '$lib/components/layout/layoutDataStore.js';
+	import PrimaryLayout from "$lib/components/layout/PrimaryLayout.svelte";
+	import { NavigationOption } from "$lib/components/layout/layoutDataStore.js";
 
-
-	export let data
+	export let data;
 </script>
 
 <!-- SEO -->
@@ -13,25 +12,35 @@
 	<meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<PrimaryLayout navigation_option={NavigationOption.Blog} fluid_sim_background={false}>
+<PrimaryLayout
+	navigation_option={NavigationOption.Blog}
+	fluid_sim_background={false}
+>
 	<article>
 		<!-- Title -->
-		  <hgroup>
-			  <h1>{data.meta.title}</h1>
-			  <p>Published at {data.meta.date}</p>
-		  </hgroup>
-	  
+		<hgroup>
+			<h1>{data.meta.title}</h1>
+			<p>
+				{#if data.meta.authors}
+				By
+				{/if}
+				{#each data.meta.authors ?? [] as author}
+					{author}
+					{#if author !== data.meta.authors[data.meta.authors.length - 1]}, {/if}
+				{/each}
+				Published at {data.meta.date}</p>
+		</hgroup>
+
 		<!-- Post -->
-		  <div class="prose">
-			  <svelte:component this={data.content} />
-		  </div>
-	  </article>
-	  
+		<div class="prose">
+			<svelte:component this={data.content} />
+		</div>
+	</article>
 </PrimaryLayout>
 
 <style lang="scss">
 	@use "../../../variables.scss" as *;
-	@media screen and (max-width: $mobile-breakpoint) {
+	@media screen and (max-width: $tablet-breakpoint) {
 		article {
 			padding: 1.5rem 1rem;
 		}
