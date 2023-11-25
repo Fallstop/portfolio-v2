e77eed9046afdd969b56a00a66f07ffa03018fdf
Jasper M-W <contact@jmw.nz>
2023-11-26 00:50:31 +1300

    Cleaned up server secrets

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+import { env } from '$env/dynamic/private';
 
-export const GITHUB_AUTH_TOKEN = process.env.GITHUB_AUTH_TOKEN;
-export const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
+export const GITHUB_AUTH_TOKEN = env.GITHUB_AUTH_TOKEN;
+export const DISCORD_WEBHOOK = env.DISCORD_WEBHOOK;
--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,10 +1,9 @@
 import { type Actions, fail } from '@sveltejs/kit';
 import { DISCORD_WEBHOOK } from '$lib/server/env.js';
 import { accentColor, hexColourToNumber, primaryColor } from '$lib/utilities/colour';
-import { EmbedBuilder } from "@discordjs/builders";
 
 export const actions: Actions = {
-	default: async ({request, getClientAddress, fetch}) => {
+    default: async ({ request, getClientAddress, fetch }) => {
         const formData = await request.formData();
         const { name, email, message } = Object.fromEntries(formData.entries());
         const IP = getClientAddress();
@@ -12,34 +11,35 @@ export const actions: Actions = {
         if (!(name && email && message && typeof name === "string" && typeof message === "string" && typeof email === "string")) {
             return fail(400, { error: "Missing required fields." });
         }
-        
+
         if (!DISCORD_WEBHOOK) {
             console.log("WARNING: No Discord webhook found.");
             return fail(500, { error: "No Discord webhook found." });
         }
-        
+
         // I am very lazy.
-        const messageEmbed = new EmbedBuilder()
-            .setTitle("New Contact Form Submission")
-            .addFields(
+        const messageEmbed = {
+            title: 'New Contact Form Submission',
+            fields: [
                 { name: 'Name', value: name, inline: true },
                 { name: 'Email', value: email, inline: true },
-                { name: 'Message', value: message },
-            )
-            .setColor(hexColourToNumber(accentColor))
-            .setTimestamp()
-            .setFooter({ text: `IP Address ${IP}`});
+                { name: 'Message', value: message }
+            ],
+            color: hexColourToNumber(accentColor),
+            timestamp: new Date().toISOString(),
+            footer: { text: `IP Address ${IP}` }
+        }
 
         fetch(DISCORD_WEBHOOK, {
             method: "POST",
             body: JSON.stringify({
-                embeds: [messageEmbed.toJSON()]
+                embeds: [messageEmbed]
             }),
             headers: {
                 "Content-Type": "application/json"
             }
         });
 
-        return { success: true}
-	}
+        return { success: true }
+    }
 };
