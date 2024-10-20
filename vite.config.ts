c79d68f7bf961be4e24df4e3b3af87d8c87a2582
Jasper Miller-Waugh <contact@jmw.nz>
2024-10-20 21:18:48 +1300

    Switch to sass-embedded
    Super weird cloud-flare builds

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: "modern-compiler",
+        api: "modern",
         additionalData: '@use "/src/variables.scss" as *;',
       },
     },
