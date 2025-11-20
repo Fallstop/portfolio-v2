1676afaa784f81ea085daba5e25f1a29a360df24
Jasper Miller-Waugh <contact@jmw.nz>
2025-11-20 21:00:06 +1300

    Fix trusted origin issue

--- svelte.config.js
+++ svelte.config.js
@@ -31,11 +31,18 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['/sitemap.xml', '<all>']
-			}
+      routes: {
+        include: ['/*'],
+        exclude: ['/sitemap.xml', '<all>']
+      }
     }),
+    csrf: {
+      trustedOrigins: [
+        "https://jmw.nz",
+        "https://*.portfolio-v2-2tk.pages.dev",
+        "http://localhost"
+      ]
+    },
     alias: {
       $md: "./src/lib/components/markdown/ProjectAssets",
     }
